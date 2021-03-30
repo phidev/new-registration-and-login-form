41353c11ccd43e2129c06fd968b07db01a02fd62
Philipp Schadt <philippschadt@gmx.de>
2021-03-30 22:14:56 +0200

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
+function ValidPassword() {
+  let password = document.getElementById("password").value;
+  let regex = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,}$)"
+  ).test(password);
+
+  if (regex) {
+    console.log("Match");
+  } else {
+    console.log("No Match");
+  }
+}
+
+function ValidEmail() {
+  let email = document.getElementById("email").value;
+  let regex = RegExp("^[^s@]+@[^s@]+.[^s@]+$").test(email); // Matcht nicht !
+
+  if (regex) {
+    document.getElementById("email").style.backgroundColor = "green";
+  } else {
+    document.getElementById("email").style.backgroundColor = "red";
+  }
+}
+
 $(document).ready(function () {
-  $("p").click(function () { 
-    $(".impressum").hide("slow");
-    
+  $("p").click(function () {
+    $(".imprint").hide("slow");
   });
-  
 });
+
+//new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
