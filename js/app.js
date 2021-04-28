a3844921d7eb0069b2fb88dd33c9cd17f939235a
Philipp Schadt <philippschadt@gmx.de>
2021-04-28 21:07:42 +0200

    Initial README

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-function ValidPassword() {
-  let password = document.getElementById("password").value;
-  let regex = new RegExp(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,}$)"
-  ).test(password);
-
+function ValidEmail() {
+  let email = document.getElementById("email").value;
+  let regex = RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$").test(email); // Matcht nicht. Nach .de .com .deineMutter soll kein Leerzeichen erlaubt sein.
   if (regex) {
     console.log("Match");
   } else {
     console.log("No Match");
   }
 }
-function ValidEmail() {
-  let email = document.getElementById("email").value;
-  let regex = RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$").test(email); // Matcht nicht. Nach .de .com .deineMutter soll kein Leerzeichen erlaubt sein.
+function ValidPassword() {
+  let password = document.getElementById("password").value;
+  let regex = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,}$)"
+  ).test(password);
+
   if (regex) {
     console.log("Match");
   } else {
